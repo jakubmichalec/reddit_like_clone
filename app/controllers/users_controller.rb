ac7a6368b7e8e211aa295b65f901e10e1e4fa497
wwilyW <kuba.michalec@gmail.com>
2015-08-22 22:58:40 +0200

    Clean User controller with decent_exposure

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,13 @@
 class UsersController < ApplicationController
 
+  expose(:user, attributes: :permitted_params)
+
   def new
-    @user = User.new
   end
 
   def create
-    @user = User.new(permitted_params)
-
-    if @user.save
-      session[:user_id] = @user.id
+    if user.save
+      session[:user_id] = user.id
       flash[:notice] = "Thank you for signing up"
       redirect_to root_url
     else
