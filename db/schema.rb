2f69a1544be04e58ea56eac415b53808fb1e26d9
wwilyW <kuba.michalec@gmail.com>
2015-09-07 23:59:57 +0200

    Add admin option to User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150831193434) do
+ActiveRecord::Schema.define(version: 20150907215832) do
 
   create_table "links", force: :cascade do |t|
     t.string   "title"
@@ -28,9 +28,10 @@ ActiveRecord::Schema.define(version: 20150831193434) do
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "remember_digest"
+    t.boolean  "admin",           default: false
   end
 
 end
